c7c7e421b4868e860c2d119a6b2cdb17d6791562
Olivier Colombani <olcolombani@gmail.com>
2019-02-15 11:18:12 +0100

    css

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,27 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail.id)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
